c31c641d192d3b8f825abb4ef88fe851a29663cf
utky <ilyaletre@gmail.com>
2013-07-10 09:32:16 +0900

    0007解答作成完了。

--- src/main/Algorithm0007.hs
+++ src/main/Algorithm0007.hs
@@ -2,11 +2,26 @@ import System.Environment
 
 main = do args <- getArgs
           let count = read (head args) :: Integer
-          putStrLn $ show $ fibonacci count 
+          let okometsubus = listFibonacci count
+          putStrLn $ "How much grains of rice on N-th day? : " ++ (show $ last okometsubus)
+          putStrLn $ "List the amount of rice per day. : " ++ (show okometsubus)
 
+{- |
+ - 指定された項までのフィボナッチ数列をリスト形式で取得します。
+ -}
+listFibonacci :: Integer -> [Integer]
+listFibonacci 0 = (fibonacci 0) : []
+listFibonacci n = listFibonacci (n - 1) ++ (fibonacci n) : []
+
+{- |
+ - フィボナッチ数列の第n項の数値を算出します。
+ -
+ - 太閤の提示した数式をほぼそのまま
+ - Haskellの式として記述できてしまいます。
+ - ( ﾟ∀ﾟ)o彡゜Haskell! Haskell!
+ -}
 fibonacci :: Integer -> Integer
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
 
-pucci :: Integer -> Integer
